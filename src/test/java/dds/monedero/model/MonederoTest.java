2b85dc850b932167ec5c2c166df95e385453fcf6
Francisco Cicerchia <francicerchia@gmail.com>
2022-05-16 23:10:35 -0300

    Agrego y completo tests

--- src/test/java/dds/monedero/model/MonederoTest.java
+++ src/test/java/dds/monedero/model/MonederoTest.java
@@ -7,19 +7,45 @@ import dds.monedero.exceptions.SaldoMenorException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MonederoTest {
   private Cuenta cuenta;
+  private Movimiento movimiento;
+  private Movimiento otroMovimiento;
 
   @BeforeEach
   void init() {
     cuenta = new Cuenta();
+    movimiento = new Movimiento(LocalDate.now(),1000,true);
+    otroMovimiento = new Movimiento(LocalDate.now(),1200,false);
+  }
+
+  @Test
+  void agregarMovimientoDepositoACuenta() {
+    cuenta.poner(1550);
+    movimiento.agregateA(cuenta);
+    assertEquals(2550,cuenta.getSaldo());
+  }
+
+  @Test
+  void agregarMovimientoExtraccionACuenta() {
+    cuenta.poner(1550);
+    otroMovimiento.agregateA(cuenta);
+    assertEquals(350,cuenta.getSaldo());
+  }
+
+  @Test
+  void movimientoDepositadoEnFechaErronea() {
+    assertFalse(movimiento.fueDepositado(LocalDate.of(2022,3,23)));
   }
 
   @Test
   void Poner() {
     cuenta.poner(1500);
+    assertEquals(1500,cuenta.getSaldo());
   }
 
   @Test
@@ -32,6 +58,7 @@ public class MonederoTest {
     cuenta.poner(1500);
     cuenta.poner(456);
     cuenta.poner(1900);
+    assertEquals(1500 + 456 + 1900,cuenta.getSaldo());
   }
 
   @Test
